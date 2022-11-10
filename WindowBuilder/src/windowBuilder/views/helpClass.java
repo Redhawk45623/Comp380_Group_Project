1c5c6921caa6271ce031f18d35bb4f88cb72d88c
BellMatt38 <115511313+BellMatt38@users.noreply.github.com>
2022-11-09 16:36:20 -0800

    Update helpClass.java

--- WindowBuilder/src/windowBuilder/views/helpClass.java
+++ WindowBuilder/src/windowBuilder/views/helpClass.java
@@ -10,6 +10,9 @@ import java.awt.Font;
 import java.awt.SystemColor;
 import javax.swing.JButton;
 import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JScrollPane;
 
 /**
 * @author Matthew Bellman
@@ -38,20 +41,33 @@ public class helpClass extends JPanel {
 		
 		JButton btnSend = new JButton("Send");
 		
+		btnSend.addActionListener(new ActionListener() {
+			
+			// Send Button Clicked
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Hello World"); // TEMP: BUTTON CLICK CHECKER
+				
+				// Email Address Checker
+				// Email Content Checker
+				// Write file (send)
+				// Display "Send confirmation" message
+				// Clear fields and open Home Pane
+				
+			}
+			
+		});
+				
 		JLabel lblNewLabel_2 = new JLabel("Or you can contact us directly by filling out the form below:");
 		
 		addressField = new JTextField();
 		
-		JLabel lblNewLabel_3 = new JLabel("Email Address: *");
-		
-		JTextArea emailArea = new JTextArea();
-		emailArea.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		emailArea.setLineWrap(true);
-		emailArea.setWrapStyleWord(true);
+		JLabel lblNewLabel_3 = new JLabel("* Email Address:");
 		
 		JLabel lblNewLabel_4 = new JLabel("* Required Field");
 		
-		JLabel lblNewLabel_5 = new JLabel("Email Meassage: *");
+		JLabel lblNewLabel_5 = new JLabel("* Email Meassage:");
+		
+		JScrollPane scrollPane = new JScrollPane();
 		
 		
 		GroupLayout groupLayout = new GroupLayout(this);
@@ -59,29 +75,29 @@ public class helpClass extends JPanel {
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
 					.addContainerGap()
-					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
-						.addGroup(groupLayout.createSequentialGroup()
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
 							.addComponent(lblNewLabel_5)
 							.addContainerGap(467, Short.MAX_VALUE))
-						.addGroup(groupLayout.createSequentialGroup()
+						.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
 							.addComponent(lblNewLabel_2)
 							.addContainerGap(271, Short.MAX_VALUE))
-						.addGroup(groupLayout.createSequentialGroup()
+						.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
 							.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
-								.addGroup(groupLayout.createSequentialGroup()
+								.addComponent(scrollPane, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 531, Short.MAX_VALUE)
+								.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
 									.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 175, GroupLayout.PREFERRED_SIZE)
 									.addPreferredGap(ComponentPlacement.RELATED, 192, Short.MAX_VALUE)
 									.addComponent(lblNewLabel_1, GroupLayout.PREFERRED_SIZE, 164, GroupLayout.PREFERRED_SIZE))
-								.addComponent(txtrIfYouHave)
 								.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
 									.addComponent(lblNewLabel_4)
 									.addPreferredGap(ComponentPlacement.RELATED, 397, Short.MAX_VALUE)
 									.addComponent(btnSend))
-								.addComponent(emailArea, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 531, Short.MAX_VALUE)
-								.addGroup(groupLayout.createSequentialGroup()
+								.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
 									.addComponent(lblNewLabel_3)
 									.addPreferredGap(ComponentPlacement.RELATED)
-									.addComponent(addressField, GroupLayout.DEFAULT_SIZE, 448, Short.MAX_VALUE)))
+									.addComponent(addressField, GroupLayout.DEFAULT_SIZE, 448, Short.MAX_VALUE))
+								.addComponent(txtrIfYouHave, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 531, Short.MAX_VALUE))
 							.addGap(24))))
 		);
 		groupLayout.setVerticalGroup(
@@ -102,13 +118,19 @@ public class helpClass extends JPanel {
 					.addGap(7)
 					.addComponent(lblNewLabel_5)
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(emailArea, GroupLayout.PREFERRED_SIZE, 357, GroupLayout.PREFERRED_SIZE)
+					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 357, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.RELATED)
 					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
 						.addComponent(lblNewLabel_4)
 						.addComponent(btnSend))
 					.addContainerGap())
 		);
+		
+		JTextArea emailArea = new JTextArea();
+		emailArea.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		emailArea.setWrapStyleWord(true);
+		emailArea.setLineWrap(true);
+		scrollPane.setViewportView(emailArea);
 		setLayout(groupLayout);
 	
 	}
